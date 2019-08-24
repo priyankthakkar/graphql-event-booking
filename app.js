f407f4d2ced8cc183b1ce6c18269afef803bf060
Priyank Thakkar <priyankthakkar@gmail.com>
2019-08-25 00:48:01 +1000

    Change Log: 1. Basic event type is created, now graphql and query and create a complex event object. Change Log: Priyank Thakkar

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
+const {
+    buildSchema
+} = require('graphql');
+
+const events = [];
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String
+        }
+
         type RootQuery {
-            events: [String!]
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -23,16 +43,23 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return [
-                'Romantic', 'Cooking', 'Sailing', 'All Night Coding'
-            ]
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            }
+
+            events.push(event)
+            return event;
         }
     },
     graphiql: true
 }));
 
-app.listen(3000)
+app.listen(3000,
+    () => console.log('server is listening at http://localhost:3000'))
